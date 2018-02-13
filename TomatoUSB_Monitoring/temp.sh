52e4d99265701265cb187fdcde37fa7f68b3a6af
vaxxi <github@vaxxi.net>
2018-02-13 12:19:25 +0200

    Update for temperature script

--- TomatoUSB_Monitoring/temp.sh
+++ TomatoUSB_Monitoring/temp.sh
@@ -3,10 +3,9 @@
 dir=`dirname $0`
 
 name="router_temp"
-columns="temp_24 temp_50"
+columns="temp_24"
 p1=`wl -i eth1 phy_tempsense | awk '{ print $1 * .5 + 20 }'` # 2.4GHz
-p2=`wl -i eth2 phy_tempsense | awk '{ print $1 * .5 + 20 }'` # 5.0GHz
-points="$p1 $p2"
+points="$p1"
 echo "$name" "$columns" "$points"
 
 $dir/todb.sh "$name" "$columns" "$points"
